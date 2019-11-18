351a6374cb4ddaa249ce4a6933b2ce2a39d1474f
Cassie Boyd <cassandra.bacon@gmail.com>
2019-11-18 14:58:17 -0600

    Work done with Jissie in class

--- src/components/Kennel.js
+++ src/components/Kennel.js
@@ -1,37 +1,23 @@
-import React, { Component } from 'react'
-import './Kennel.css'
-import AnimalCard from './animal/AnimalCard'
-import OwnerCard from './owners/OwnerCard';
-import LocationCard from './locations/LocationCard';
-import EmployeeCard from './employees/EmployeeCard';
-import NavBar from './nav/NavBar';
-
+import React, { Component } from "react";
+import "./Kennel.css";
+import AnimalCard from "./animal/AnimalCard";
+import OwnerCard from "./owners/OwnerCard";
+import LocationCard from "./locations/LocationCard";
+import EmployeeCard from "./employees/EmployeeCard";
+import NavBar from "./nav/NavBar";
+import ApplicationViews from "./ApplicationViews";
 
 // Kennel is an object
 class Kennel extends Component {
-    // render() is a method on the Kennel object that returns an HTML representation of some (hard-coded) data.
-    render() {
-        return (
-            <div>
-                <NavBar />
-                <h2>Student Kennels<br />
-                    <small>Loving care when you're not there.</small>
-                </h2>
-                <address>
-                    Visit Us at the Nashville North Location
-                    <br />500 Puppy Way
-                </address>
-                <div>
-                    <AnimalCard />
-                    <OwnerCard />
-                    <LocationCard />
-                    <EmployeeCard />
-                    <AnimalCard />
-                    <AnimalCard />
-                </div>
-            </div>
-        );
-    }
+  // render() is a method on the Kennel object that returns an HTML representation of some (hard-coded) data.
+  render() {
+    return (
+      <React.Fragment>
+        <NavBar />
+        <ApplicationViews />
+      </React.Fragment>
+    );
+  }
 }
 
-export default Kennel
+export default Kennel;
